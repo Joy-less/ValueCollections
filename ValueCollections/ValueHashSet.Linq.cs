4e935f276b7afe19b6c00bc73fd2c92352473ca6
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-03-31 06:47:56 +0100

    Fix ValueHashSet LINQ

--- ValueCollections/ValueHashSet.Linq.cs
+++ ValueCollections/ValueHashSet.Linq.cs
@@ -6,73 +6,73 @@ partial struct ValueHashSet<T> {
     /// <inheritdoc cref="ValueList{T}.Where(Func{T, bool})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Where(Func<T, bool> predicate) {
-        return ValueList<T>.FromBuffer(Buffer).Where(predicate);
+        return this.ToValueList().Where(predicate);
     }
 
     /// <inheritdoc cref="ValueList{T}.Select(Func{T, T})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Select(Func<T, T> selector) {
-        return ValueList<T>.FromBuffer(Buffer).Select(selector);
+        return this.ToValueList().Select(selector);
     }
 
     /// <inheritdoc cref="ValueList{T}.Select(Func{T, int, T})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Select(Func<T, int, T> selector) {
-        return ValueList<T>.FromBuffer(Buffer).Select(selector);
+        return this.ToValueList().Select(selector);
     }
 
     /// <inheritdoc cref="ValueList{T}.SelectMany(Func{T, IEnumerable{T}})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> SelectMany(Func<T, IEnumerable<T>> selector) {
-        return ValueList<T>.FromBuffer(Buffer).SelectMany(selector);
+        return this.ToValueList().SelectMany(selector);
     }
 
     /// <inheritdoc cref="ValueList{T}.SelectMany(Func{T, int, IEnumerable{T}})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> SelectMany(Func<T, int, IEnumerable<T>> selector) {
-        return ValueList<T>.FromBuffer(Buffer).SelectMany(selector);
+        return this.ToValueList().SelectMany(selector);
     }
 
     /// <inheritdoc cref="ValueList{T}.Append(T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Append(T value) {
-        return ValueList<T>.FromBuffer(Buffer).Append(value);
+        return this.ToValueList().Append(value);
     }
 
     /// <inheritdoc cref="ValueList{T}.Prepend(T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Prepend(T value) {
-        return ValueList<T>.FromBuffer(Buffer).Prepend(value);
+        return this.ToValueList().Prepend(value);
     }
 
     /// <inheritdoc cref="ValueList{T}.Reverse()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Reverse() {
-        return ValueList<T>.FromBuffer(Buffer).Reverse();
+        return this.ToValueList().Reverse();
     }
 
     /// <inheritdoc cref="ValueList{T}.OfType{TFilter}()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<TFilter> OfType<TFilter>() {
-        return ValueList<T>.FromBuffer(Buffer).OfType<TFilter>();
+        return this.ToValueList().OfType<TFilter>();
     }
 
     /// <inheritdoc cref="ValueList{T}.Cast{TResult}()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<TResult> Cast<TResult>() {
-        return ValueList<T>.FromBuffer(Buffer).Cast<TResult>();
+        return this.ToValueList().Cast<TResult>();
     }
 
     /// <inheritdoc cref="ValueList{T}.Except(T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Except(T value) {
-        return ValueList<T>.FromBuffer(Buffer).Except(value);
+        return this.ToValueList().Except(value);
     }
 
     /// <inheritdoc cref="ValueList{T}.Except(ReadOnlySpan{T})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Except(scoped ReadOnlySpan<T> values) {
-        return ValueList<T>.FromBuffer(Buffer).Except(values);
+        return this.ToValueList().Except(values);
     }
 
     /// <inheritdoc cref="ValueList{T}.Except(ReadOnlyMemory{T})"/>
@@ -81,7 +81,7 @@ partial struct ValueHashSet<T> {
     [OverloadResolutionPriority(-1)]
 #endif
     public readonly ValueList<T> Except(ReadOnlyMemory<T> values) {
-        return ValueList<T>.FromBuffer(Buffer).Except(values.Span);
+        return this.ToValueList().Except(values.Span);
     }
 
     /// <inheritdoc cref="ValueList{T}.Except(IEnumerable{T})"/>
@@ -90,144 +90,144 @@ partial struct ValueHashSet<T> {
     [OverloadResolutionPriority(-2)]
 #endif
     public readonly ValueList<T> Except(IEnumerable<T> values) {
-        return ValueList<T>.FromBuffer(Buffer).Except(values);
+        return this.ToValueList().Except(values);
     }
 
     /// <inheritdoc cref="ValueList{T}.Order()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Order() {
-        return ValueList<T>.FromBuffer(Buffer).Order();
+        return this.ToValueList().Order();
     }
 
     /// <inheritdoc cref="ValueList{T}.Order{TComparer}(TComparer)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> Order<TComparer>(TComparer comparer) where TComparer : IComparer<T> {
-        return ValueList<T>.FromBuffer(Buffer).Order(comparer);
+        return this.ToValueList().Order(comparer);
     }
 
     /// <inheritdoc cref="ValueList{T}.OrderDescending()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> OrderDescending() {
-        return ValueList<T>.FromBuffer(Buffer).OrderDescending();
+        return this.ToValueList().OrderDescending();
     }
 
     /// <inheritdoc cref="ValueList{T}.OrderDescending{TComparer}(TComparer)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> OrderDescending<TComparer>(TComparer comparer) where TComparer : IComparer<T> {
-        return ValueList<T>.FromBuffer(Buffer).OrderDescending(comparer);
+        return this.ToValueList().OrderDescending(comparer);
     }
 
     /// <inheritdoc cref="ValueList{T}.OrderBy{TKey}(Func{T, TKey})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> OrderBy<TKey>(Func<T, TKey> keySelector) {
-        return ValueList<T>.FromBuffer(Buffer).OrderBy(keySelector);
+        return this.ToValueList().OrderBy(keySelector);
     }
 
     /// <inheritdoc cref="ValueList{T}.OrderBy{TKey, TComparer}(Func{T, TKey}, TComparer)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> OrderBy<TKey, TComparer>(Func<T, TKey> keySelector, TComparer comparer) where TComparer : IComparer<TKey> {
-        return ValueList<T>.FromBuffer(Buffer).OrderBy(keySelector, comparer);
+        return this.ToValueList().OrderBy(keySelector, comparer);
     }
 
     /// <inheritdoc cref="ValueList{T}.OrderByDescending{TKey}(Func{T, TKey})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> OrderByDescending<TKey>(Func<T, TKey> keySelector) {
-        return ValueList<T>.FromBuffer(Buffer).OrderByDescending(keySelector);
+        return this.ToValueList().OrderByDescending(keySelector);
     }
 
     /// <inheritdoc cref="ValueList{T}.OrderByDescending{TKey, TComparer}(Func{T, TKey}, TComparer)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly ValueList<T> OrderByDescending<TKey, TComparer>(Func<T, TKey> keySelector, TComparer comparer) where TComparer : IComparer<TKey> {
-        return ValueList<T>.FromBuffer(Buffer).OrderByDescending(keySelector, comparer);
+        return this.ToValueList().OrderByDescending(keySelector, comparer);
     }
 
     /// <inheritdoc cref="ValueList{T}.All(Func{T, bool})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly bool All(Func<T, bool> predicate) {
-        return ValueList<T>.FromBuffer(Buffer).All(predicate);
+        return this.ToValueList().All(predicate);
     }
 
     /// <inheritdoc cref="ValueList{T}.Any()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly bool Any() {
-        return ValueList<T>.FromBuffer(Buffer).Any();
+        return this.ToValueList().Any();
     }
 
     /// <inheritdoc cref="ValueList{T}.Any(Func{T, bool})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly bool Any(Func<T, bool> predicate) {
-        return ValueList<T>.FromBuffer(Buffer).Any(predicate);
+        return this.ToValueList().Any(predicate);
     }
 
     /// <inheritdoc cref="ValueList{T}.First()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T First() {
-        return ValueList<T>.FromBuffer(Buffer).First();
+        return this.ToValueList().First();
     }
 
     /// <inheritdoc cref="ValueList{T}.First(Func{T, bool})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T First(Func<T, bool> predicate) {
-        return ValueList<T>.FromBuffer(Buffer).First(predicate);
+        return this.ToValueList().First(predicate);
     }
 
     /// <inheritdoc cref="ValueList{T}.FirstOrDefault(T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T? FirstOrDefault(T? defaultValue = default) {
-        return ValueList<T>.FromBuffer(Buffer).FirstOrDefault(defaultValue);
+        return this.ToValueList().FirstOrDefault(defaultValue);
     }
 
     /// <inheritdoc cref="ValueList{T}.FirstOrDefault(Func{T, bool}, T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T? FirstOrDefault(Func<T, bool> predicate, T? defaultValue = default) {
-        return ValueList<T>.FromBuffer(Buffer).FirstOrDefault(predicate, defaultValue);
+        return this.ToValueList().FirstOrDefault(predicate, defaultValue);
     }
 
     /// <inheritdoc cref="ValueList{T}.Last()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T Last() {
-        return ValueList<T>.FromBuffer(Buffer).Last();
+        return this.ToValueList().Last();
     }
 
     /// <inheritdoc cref="ValueList{T}.Last(Func{T, bool})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T Last(Func<T, bool> predicate) {
-        return ValueList<T>.FromBuffer(Buffer).Last(predicate);
+        return this.ToValueList().Last(predicate);
     }
 
     /// <inheritdoc cref="ValueList{T}.LastOrDefault(T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T? LastOrDefault(T? defaultValue = default) {
-        return ValueList<T>.FromBuffer(Buffer).LastOrDefault(defaultValue);
+        return this.ToValueList().LastOrDefault(defaultValue);
     }
 
     /// <inheritdoc cref="ValueList{T}.LastOrDefault(Func{T, bool}, T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T? LastOrDefault(Func<T, bool> predicate, T? defaultValue = default) {
-        return ValueList<T>.FromBuffer(Buffer).LastOrDefault(predicate, defaultValue);
+        return this.ToValueList().LastOrDefault(predicate, defaultValue);
     }
 
     /// <inheritdoc cref="ValueList{T}.Single()"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T Single() {
-        return ValueList<T>.FromBuffer(Buffer).Single();
+        return this.ToValueList().Single();
     }
 
     /// <inheritdoc cref="ValueList{T}.Single(Func{T, bool})"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T Single(Func<T, bool> predicate) {
-        return ValueList<T>.FromBuffer(Buffer).Single(predicate);
+        return this.ToValueList().Single(predicate);
     }
 
     /// <inheritdoc cref="ValueList{T}.SingleOrDefault(T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T? SingleOrDefault(T? defaultValue = default) {
-        return ValueList<T>.FromBuffer(Buffer).SingleOrDefault(defaultValue);
+        return this.ToValueList().SingleOrDefault(defaultValue);
     }
 
     /// <inheritdoc cref="ValueList{T}.SingleOrDefault(Func{T, bool}, T)"/>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public readonly T? SingleOrDefault(Func<T, bool> predicate, T? defaultValue = default) {
-        return ValueList<T>.FromBuffer(Buffer).SingleOrDefault(predicate, defaultValue);
+        return this.ToValueList().SingleOrDefault(predicate, defaultValue);
     }
 }
